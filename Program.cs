548491503484935f2bb52a6f2a0e0d99de38cb77
MuhamamdUzair <75124050+MuhamamdUzair@users.noreply.github.com>
2020-12-04 18:05:28 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+namespace ThreadingSyntax
+{
+    class Program
+    {
+       static public Thread oThread = new Thread(MethodJoin);
+        static public Thread oThread1 = new Thread(MethodJoin1);
+        static void Main(string[] args)
+        {
+            oThread.Start();
+            oThread.Join();
+            oThread1.Start();
+            oThread1.Join();
+           // oThread1.Join();
+            Console.WriteLine("work completed..!");
+            
+
+        }
+    
+
+            static void MethodJoin()
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    Console.WriteLine("work is in progress..1");
+                  
+
+                }
+            Monitor.Wait(oThread);
+
+
+
+        }
+            static void MethodJoin1()
+            {
+                Monitor.Pulse(oThread);
+                for (int i = 0; i <= 100; i++)
+                {
+                    Console.WriteLine("work is in progress..2");
+
+                }
+
+
+            }
+        
+
+
+    }
+}
